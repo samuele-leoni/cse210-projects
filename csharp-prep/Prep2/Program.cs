4abe31a5a52af4b7e62f42b32d51438536ae908d
samuele-leoni <leo22009@byui.edu>
2023-01-03 17:10:23 +0100

    Finished C# Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Insert your grade percentage: ");
+        int gradeNum = int.Parse(Console.ReadLine());
+        string gradeStr = "";
+
+        if(gradeNum >= 90)
+        {
+            gradeStr = "A";
+        }
+        else if(gradeNum >= 80)
+        {
+            gradeStr = "B";
+        }
+        else if(gradeNum >= 70)
+        {
+            gradeStr = "C";
+        }
+        else if(gradeNum >= 60)
+        {
+            gradeStr = "D";
+        }
+        else
+        {
+            gradeStr = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeStr}.");
+        Console.WriteLine(gradeNum >= 70?"Congratulations! You passed the class!":"Unfortunately you didn't pass.");
     }
 }
