ab2c48c6c9dbc0260a43f632e9491f942e711731
samuele-leoni <leo22009@byui.edu>
2023-02-01 22:07:50 +0100

    Finished Fraction Assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new(), f2 = new(5), f3 = new(3, 4), f4 = new(1, 3);
+
+        Console.WriteLine($"{f1.GetFractionString()}\n{f1.GetDecimalValue()}\n{f2.GetFractionString()}\n{f2.GetDecimalValue()}\n{f3.GetFractionString()}\n{f3.GetDecimalValue()}\n{f4.GetFractionString()}\n{f4.GetDecimalValue()}");
     }
 }
