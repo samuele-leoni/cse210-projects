1e6e569667b0b0aa9e1553dad84657772fdfd598
samuele-leoni <leo22009@byui.edu>
2023-02-13 23:24:04 +0100

    TempCommit

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,30 @@
+class Reference {
+    
+    private string _volume;
+    private string _book;
+    private int _chapter;
+    private int _firstVerse;
+    private int _lastVerse;
+
+    public Reference()
+    {
+        
+    }
+
+    public Reference(string volume, string book, int chapter, int verse)
+    {
+        
+    }
+
+    public Reference(string volume, string book, int chapter, int firstVerse, int endVerse)
+    {
+
+    }
+
+    
+
+    public void Display()
+    {
+
+    }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,44 @@
+class Word
+{
+    private string _word;
+    private bool _isHidden;
+
+    public Word(string word)
+    {
+        _word = word;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+    
+    public string GetRenderedText()
+    {
+        string val = "";
+        
+        if (_isHidden)
+        {
+            foreach (char c in _word)
+            {
+                val += '_';
+            }
+        }
+        else
+        {
+            val = _word;
+        }
+
+        return val;
+    }
+}
