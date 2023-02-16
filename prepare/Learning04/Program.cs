222b941b6bfb2dc00d5f48bae2388080a2a1639d
samuele-leoni <leo22009@byui.edu>
2023-02-16 22:31:04 +0100

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine($"\nExample 1:\n{a1.GetSummary()}");
+        Console.WriteLine($"\nExample 2:\n{a2.GetSummary()}\n{a2.GetHomeworkList()}");
+        Console.WriteLine($"\nExample 3:\n{a3.GetSummary()}\n{a3.GetWritingInformation()}\n");
     }
 }
